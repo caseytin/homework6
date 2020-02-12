48ac0454481e0612a665a9accb6f3f9c5c842f1f
Casey Tin <ctin@umich.edu>
2020-02-12 13:26:35 -0500

    testing js file linking

---
+++ js/video.js
@@ -0,0 +1,54 @@
+var video;
+
+function getVid(){
+	video = document.querySelector("#myVideo"); 
+}
+
+function playVid() { 
+	console.log("Play Video");
+	video = document.querySelector("#myVideo"); 
+	video.play()
+} 
+
+function pauseVid() { 
+	console.log("Pause Video");
+	video = document.querySelector("#myVideo"); 
+	video.pause()
+} 
+
+function decreaseSpeed() { 
+
+  	console.log("Speed is ");
+} 
+
+function increaseSpeed() {
+
+	console.log("Speed is "+ );
+} 
+
+function skipAhead() {
+
+	console.log("Current location is "+ );
+} 
+
+function mute() { 
+  	
+  		console.log("Unmuted");
+  	
+      	console.log("Muted");
+}
+
+function changeVolume() {
+;	console.log("Volume is ");
+}
+       
+
+function gray() { 
+
+	console.log("In grayscale")
+}
+
+function color() {
+
+	console.log("In color") 
+}
